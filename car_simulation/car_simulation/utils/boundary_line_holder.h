206f08c121dde55703da22a03ee318059b70335b
istrandjev@gmail.com <istrandjev@gmail.com@28d065b4-702d-1fc3-fb8c-6eee9887d1d4>
2012-02-25 11:25:28 +0000

    Adding boundary_line_holder class.

---
+++ car_simulation/car_simulation/utils/boundary_line_holder.h
@@ -0,0 +1,26 @@
+#ifndef CAR_SIMULATION_CAR_SIMULATION_BOUNDARY_LINES_HOLDER_H
+#define CAR_SIMULATION_CAR_SIMULATION_BOUNDARY_LINES_HOLDER_H
+
+#include <vector>
+
+namespace geometry { 
+class BoundaryLine;  
+class Segment;
+}  // namespace geometry
+
+namespace utils {
+
+class BoundaryLinesHolder {
+ public:
+  ~BoundaryLinesHolder();
+
+  geometry::BoundaryLine* AddStraightBoundaryLine(
+      const geometry::Segment& line, bool crossable);
+
+ private:
+  std::vector<geometry::BoundaryLine*> boundaryLines_;
+};
+
+}  // namespace utils
+
+#endif CAR_SIMULATION_CAR_SIMULATION_BOUNDARY_LINES_HOLDER_H
