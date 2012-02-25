ed99735ce7d0c9be264facbf01b6f5c89c9522e5
istrandjev@gmail.com <istrandjev@gmail.com@28d065b4-702d-1fc3-fb8c-6eee9887d1d4>
2012-02-25 11:24:00 +0000

    * Adding more functionality to segment:   - Get Point by fraction   - Intersect two segments * Adding more functionality to double utils - check if a value is in a given interval

--- trunk/geometry/segement.cpp
+++ trunk/geometry/segement.cpp
@@ -2,6 +2,8 @@
 
 #include "geometry/bounding_box.h"
 #include "geometry/point.h"
+#include "geometry/vector.h"
+#include "utils/double_utils.h"
 
 #include <algorithm>
 
@@ -28,6 +30,46 @@ BoundingBox Segment::GetBoundingBox() const {
   maxy = std::max(A_.y, B_.y);
   
   return BoundingBox(minx, maxx, miny, maxy);
+}
+
+bool Segment::Intersect(const Segment& other, double* this_fraction,
+    double* other_fraction) const {
+  geometry::Vector vector1(A(), B());
+  geometry::Vector vector2(other.A(), other.B());
+
+  double determinant = vector2.CrossProduct(vector1);
+  if (DoubleIsZero(determinant)) {
+    return false;
+  }
+
+  double x_diff = A().x - other.A().x;
+  double y_diff = A().y - other.A().y;
+
+  *this_fraction = (vector2.y * x_diff - vector2.x * y_diff) / determinant;
+  if (!DoubleIsBetween(*this_fraction, 0.0, 1.0)) {
+    return false;
+  }
+
+  *other_fraction = (vector1.y * x_diff - vector1.x * y_diff) / determinant;
+  if (!DoubleIsBetween(*other_fraction, 0.0, 1.0)) {
+    return false;
+  }
+
+  return true;
+}
+
+bool Segment::Intersect(const Segment& other, double* this_fraction) const {
+  double other_fraction;
+  return Intersect(other, this_fraction, &other_fraction);
+}
+
+Point Segment::GetPoint(double fraction) const {
+  return Point(A_.x * (1.0 - fraction) + B_.x * fraction,
+               A_.y * (1.0 - fraction) + B_.y * fraction);
+}
+
+Segment Segment::SubSegment(double start_fraction, double end_fraction) const {
+  return Segment(GetPoint(start_fraction), GetPoint(end_fraction));
 }
 
 }  // namespace geometry
