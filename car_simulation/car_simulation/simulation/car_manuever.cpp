106c99855b8882edda0c14ea4e4c8a2e3827a587
istrandjev@gmail.com <istrandjev@gmail.com@28d065b4-702d-1fc3-fb8c-6eee9887d1d4>
2012-04-16 14:49:17 +0000

    Added initial implementation for the routing algorithm. Using dijkstra's algorithm for all the positions in the graph.

--- car_simulation/car_simulation/simulation/car_manuever.cpp
+++ car_simulation/car_simulation/simulation/car_manuever.cpp
@@ -1,17 +1,24 @@
 #include "simulation/car_manuever.h"
 
+#include "simulation/car.h"
+
 namespace simulation {
 
-CarManuever::CarManuever(const geometry::Point& rotation_center,
-    double steering_angle, double distance)
-        : rotationCenter_(rotation_center),
-        steeringAngle_(steering_angle),
-        distance_(distance) {}
+CarManuever::CarManuever(
+    const simulation::Car& begin_position,
+    const geometry::Point& rotation_center,
+    double steering_angle,
+    double distance)
+        : beginPosition_(begin_position),
+          rotationCenter_(rotation_center),
+          steeringAngle_(steering_angle),
+          distance_(distance) {}
 
-CarManuever::CarManuever(double distance)
-  : steeringAngle_(0.0), distance_(distance) {}
+CarManuever::CarManuever(
+    const simulation::Car& begin_position, double distance)
+  : beginPosition_(begin_position), steeringAngle_(0.0), distance_(distance) {}
 
-CarManuever::CarManuever() : steeringAngle_(0.0), distance_(0.0) {}
+CarManuever::CarManuever() : beginPosition_(0.0, 0.0, 0.0), steeringAngle_(0.0), distance_(0.0) {}
 
 void CarManuever::SetSteeringAngle(double steering_angle) {
   steeringAngle_ = steering_angle;
@@ -35,6 +42,14 @@ void CarManuever::SetRotationCenter(const geometry::Point& center) {
 
 const geometry::Point& CarManuever::GetRotationCenter() const {
   return rotationCenter_;
+}
+
+void CarManuever::SetBeginPosition(const simulation::Car& begin_position) {
+  beginPosition_ = begin_position;
+}
+
+simulation::Car CarManuever::GetBeginPosition() const {
+  return beginPosition_;
 }
 
 }  // namespace simulation
---
+++ car_simulation/car_simulation/simulation/car_positions_graph_router.cpp
@@ -0,0 +1,82 @@
+#include "simulation/car_positions_graph_router.h"
+
+#include "simulation/car.h"
+#include "simulation/car_manuever.h"
+#include "simulation/car_positions_graph.h"
+#include "utils/double_utils.h"
+
+#include <queue>
+
+using namespace std;
+
+namespace simulation {
+CarPositionsGraphRouter::CarPositionsGraphRouter(
+    const CarPositionsGraph *graph) : graph_(graph) {}
+
+std::vector<CarManuever> CarPositionsGraphRouter::GetRoute(
+    int from_index, const std::vector<int>& end_indices) const {
+  const vector<vector<GraphEdge> >& graph = graph_->GetGraph();
+  int n = static_cast<int>(graph.size());
+  vector<double> dist(n, -1.0);
+
+  vector<bool> end_position(n, false);
+  for (unsigned i = 0 ; i < end_indices.size(); ++i) {
+    end_position[i] = true;
+  }
+  vector<bool> visited(n, false);
+  vector<pair<int, int> > parent(n);
+  priority_queue<pair<double, int> > q;
+
+  q.push(make_pair(0, from_index));
+  parent[from_index] = make_pair(from_index, 0);
+
+  int end_index = -1;
+  while (!q.empty()) {
+    int index = q.top().second;
+    double d = -q.top().first;
+
+    q.pop();
+    if (visited[index]) {
+      continue;
+    }
+    visited[index] = true;
+
+    // Found an end position - no need to continue searching.
+    if (end_position[index]) {
+      end_index = index;
+      break;
+    }
+
+    for (unsigned i = 0; i < graph[index].size(); ++i) {
+      double new_dist = d + graph[index][i].second.GetDistance();
+      int neighbour_index = graph[index][i].first;
+      if (visited[neighbour_index]) {
+        continue;
+      }
+      if (dist[neighbour_index] < 0 ||
+          DoubleIsGreater(dist[neighbour_index], new_dist)) {
+        parent[neighbour_index] = make_pair(index, i);
+        dist[neighbour_index] = new_dist;
+        q.push(make_pair(-dist[neighbour_index], neighbour_index));
+      }
+    }
+  }
+  vector<CarManuever> result;
+
+  // No route to end position found
+  if (end_index == -1) {
+    return result;
+  }
+  int current = end_index;
+  while (parent[current].first != current) {
+    const GraphEdge& edge =
+        graph[parent[current].first][parent[current].second];
+    result.push_back(edge.second);
+    current = parent[current].first;
+  }
+  reverse(result.begin(), result.end());
+
+  return result;
+}
+
+}  // namespace simulation
